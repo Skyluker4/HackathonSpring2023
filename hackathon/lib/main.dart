21a32e70f5e0dfe9c37cda75e38abb97c28902f6
BatSmacker84 <ooohhhwells@gmail.com>
2023-04-01 13:51:09 -0500

    ROUTES WORK

--- hackathon/lib/main.dart
+++ hackathon/lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/services.dart'
     show DeviceOrientation, SystemChrome, rootBundle;
 import 'package:flutter/material.dart';
+import 'package:google_map_polyline_new/google_map_polyline_new.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -79,7 +81,8 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     _loadMapStyles();
     _getLocation();
     loadData();
-    timer = Timer.periodic(const Duration(seconds: 60), (Timer t) => loadData());
+    timer =
+        Timer.periodic(const Duration(seconds: 60), (Timer t) => loadData());
     BitmapDescriptor.fromAssetImage(
             const ImageConfiguration(devicePixelRatio: 2.5),
             'assets/images/trash-resize.png')
@@ -93,8 +96,6 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       recycleIcon = onValue;
     });
   }
-
-
 
   _getLocation() async {
     // WidgetsBinding.instance.addPostFrameCallback((_) async {
@@ -257,7 +258,12 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   Widget _backButton() {
     return FloatingActionButton(
-      onPressed: _mainScene,
+      onPressed: () {
+        if (_polylines.isNotEmpty) {
+          _polylines.clear();
+        }
+        _mainScene();
+      },
       child: const Icon(Icons.arrow_back),
     );
   }
@@ -307,7 +313,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   Widget findTrash() {
     return FloatingActionButton.extended(
       onPressed: () {
-        _locateScene();
+        computePath(PinType.garbage);
       },
       label: const Text(
         'Trash',
@@ -320,7 +326,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   Widget findRecycle() {
     return FloatingActionButton.extended(
       onPressed: () {
-        _locateScene();
+        computePath(PinType.recycle);
       },
       label: const Text(
         'Recycle',
@@ -423,6 +429,10 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   final List<Marker> _markers = <Marker>[];
   final List<Pin> _pins = <Pin>[];
+  final List<Polyline> _polylines = <Polyline>[];
+
+  GoogleMapPolyline googleMapPolyline =
+      GoogleMapPolyline(apiKey: "AIzaSyCRqFm9BmR_OSO8GL7G6E2CC295MgjDJ28");
 
   loadData() async {
     final pins = await getPins();
@@ -440,6 +450,110 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       ));
       setState(() {});
     }
+  }
+
+  // Calculate the distance between two points with the pythagorian theorem
+  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    final distance = sqrt(pow(lat2 - lat1, 2) + pow(lon2 - lon1, 2));
+    return distance;
+  }
+
+  // Loop over the list of pins and see which one is closest to the user
+  // Pass in pin type to filter by
+  Pin findClosestPin(PinType? type) {
+    late Pin closestPin;
+    double closestDistance = double.infinity;
+    if (type == PinType.garbage) {
+      for (var i = 0; i < _pins.length; i++) {
+        if (_pins[i].type == PinType.garbage) {
+          final distance = calculateDistance(
+            _currentLatLng.latitude,
+            _currentLatLng.longitude,
+            _pins[i].latitude,
+            _pins[i].longitude,
+          );
+
+          if (distance < closestDistance) {
+            closestPin = _pins[i];
+            closestDistance = distance;
+          }
+        }
+      }
+    } else {
+      for (var i = 0; i < _pins.length; i++) {
+        if (_pins[i].type == PinType.recycle) {
+          final distance = calculateDistance(
+            _currentLatLng.latitude,
+            _currentLatLng.longitude,
+            _pins[i].latitude,
+            _pins[i].longitude,
+          );
+
+          if (distance < closestDistance) {
+            closestPin = _pins[i];
+            closestDistance = distance;
+          }
+        }
+      }
+    }
+
+    return closestPin;
+  }
+
+  // Match the closest pin to the user with the closest marker
+  Marker findClosestMarker(PinType? type) {
+    final closestPin = findClosestPin(type);
+    late Marker closestMarker;
+
+    for (var i = 0; i < _markers.length; i++) {
+      if (_markers[i].markerId.value == closestPin.id.toString()) {
+        closestMarker = _markers[i];
+      }
+    }
+
+    return closestMarker;
+  }
+
+  // Focus the camera on the point halfway between the user and the closest pin
+  void _locateCamera(PinType? type) {
+    final closestPin = findClosestPin(type);
+
+    final lat = (_currentLatLng.latitude + closestPin.latitude) / 2;
+    final long = (_currentLatLng.longitude + closestPin.longitude) / 2;
+
+    // Animate the camera to the new position
+    // Adjust the zoom level to fit the two points on the screen
+    googleMapController.future.then((controller) {
+      controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(lat, long),
+            zoom: 18,
+          ),
+        ),
+      );
+    });
+  }
+
+  // Draw a line between the user and the closest pin
+  computePath(PinType? type) async {
+    final arrival = findClosestMarker(type);
+    final routeCoords = <LatLng>[];
+    LatLng origin = LatLng(_currentLatLng.latitude, _currentLatLng.longitude);
+    LatLng end = LatLng(arrival.position.latitude, arrival.position.longitude);
+    routeCoords.addAll((await googleMapPolyline.getCoordinatesWithLocation(
+        origin: origin, destination: end, mode: RouteMode.walking))!);
+
+    setState(() {
+      _polylines.add(Polyline(
+          polylineId: const PolylineId('iter'),
+          visible: true,
+          points: routeCoords,
+          width: 4,
+          color: Colors.blue,
+          startCap: Cap.roundCap,
+          endCap: Cap.buttCap));
+    });
   }
 
   // Send data to the server when a new pin is added
@@ -491,6 +605,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
             zoomGesturesEnabled: true,
             zoomControlsEnabled: false,
             markers: Set<Marker>.of(_markers),
+            polylines: Set<Polyline>.of(_polylines),
             onMapCreated: (GoogleMapController controller) async {
               // to control the camera position of the map
               googleMapController.complete(controller);
