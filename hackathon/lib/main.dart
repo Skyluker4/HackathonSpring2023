d673c51009d6b04885ce47df078bfcdffab2266b
BatSmacker84 <ooohhhwells@gmail.com>
2023-04-01 05:54:18 -0500

    Basic UI
    Maps Dark/Light mode
    Buttons almost work

--- hackathon/lib/main.dart
+++ hackathon/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -53,11 +54,49 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   int _scene = 0;
+  String _darkMapStyle = '';
+  String _lightMapStyle = '';
 
-  CameraPosition _initialCameraPosition = CameraPosition(target: LatLng(20.5937, 78.9629));
-  late final GoogleMapController googleMapController;
+  final Completer<GoogleMapController> googleMapController = Completer();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _loadMapStyles();
+  }
+
+  Future _loadMapStyles() async {
+    _darkMapStyle = await rootBundle.loadString('assets/json/dark.json');
+    _lightMapStyle = await rootBundle.loadString('assets/json/light.json');
+  }
+
+  Future _setMapStyle() async {
+    final controller = await googleMapController.future;
+    final theme = WidgetsBinding.instance.window.platformBrightness;
+    if (theme == Brightness.dark)
+      controller.setMapStyle(_darkMapStyle);
+    else
+      controller.setMapStyle(_lightMapStyle);
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    setState(() {
+      _setMapStyle();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  CameraPosition _initialCameraPosition =
+      CameraPosition(target: LatLng(20.5937, 78.9629));
 
   void _mainScene() {
     setState(() {
@@ -120,26 +159,68 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget back() {
-    return Scaffold(
-        body: SafeArea(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Back button not visible if counter is 0
-          if (_scene != 0)
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios_new_rounded),
-              onPressed: _mainScene,
+  Widget _addPage() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20, left: 20),
+            child: SizedBox(
+              height: 56,
+              width: MediaQuery.of(context).size.width - 112,
+              child: FloatingActionButton.extended(
+                onPressed: _mainScene,
+                label: const Text(
+                  'Recycle Bin',
+                  style: TextStyle(fontSize: 18),
+                ),
+                icon: const Icon(Icons.recycling),
+              ),
             ),
-          // Options button (not implemented)
-          /*IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () {},
-                ),*/
-        ],
-      ),
-    ));
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: SizedBox(
+              height: 56,
+              width: MediaQuery.of(context).size.width - 112,
+              child: FloatingActionButton.extended(
+                onPressed: _mainScene,
+                label: const Text(
+                  'Garbage Bin',
+                  style: TextStyle(fontSize: 18),
+                ),
+                icon: const Icon(Icons.delete),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _back() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        // Back button not visible if counter is 0
+        if (_scene != 0)
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: FloatingActionButton(
+                onPressed: _mainScene,
+                child: const Icon(Icons.arrow_back),
+              ),
+            ),
+          ),
+      ],
+    );
   }
 
   @override
@@ -147,28 +228,34 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       extendBody: true,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            //Google Map
-            // Back button
-            back(),
-            // Main page
-            if (_scene == 0) _mainPage(),
-            GoogleMap(
-              initialCameraPosition: _initialCameraPosition ,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              mapType: MapType.normal,
-              zoomGesturesEnabled: true,
-              zoomControlsEnabled: true,
-              onMapCreated: (GoogleMapController c) {
-                // to control the camera position of the map
-                googleMapController = c;
-              },
+      body: Stack(
+        children: [
+          // Google Map
+          GoogleMap(
+            initialCameraPosition: _initialCameraPosition,
+            compassEnabled: false,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false,
+            mapType: MapType.normal,
+            zoomGesturesEnabled: true,
+            zoomControlsEnabled: false,
+            onMapCreated: (GoogleMapController controller) {
+              // to control the camera position of the map
+              googleMapController.complete(controller);
+              _setMapStyle();
+            },
+          ),
+          SafeArea(
+            child: Stack(
+              children: [
+                // Switch between scenes
+                if (_scene == 0) _mainPage(),
+                if (_scene == 1) _addPage(),
+                if (_scene == 2) _back(),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
