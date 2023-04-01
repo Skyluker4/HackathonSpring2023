715758527898d8c8eb73988b8c27605105cc3014
Luke Simmons <luke5083@live.com>
2023-03-31 22:54:38 -0500

    Add flask

--- backend/setup.py
+++ backend/setup.py
@@ -3,20 +3,12 @@
 from setuptools import setup, find_packages
 
 
-def readme():
-    """Get long description from README.md"""
-    with open('README.md') as f:
-        return f.read()
-
-
 setup(
     name="CSCE 3513 Project",
     version="0.1.0",
     author="Luke Simmons",
     author_email="luke5083@live.com",
     description="Hackathon Spring 2023 backend server.",
-    long_description=readme(),
-    long_description_content_type="text/markdown",
     url="https://github.com/Skyluker4/HackathonSpring2023",
     packages=find_packages(),
     classifiers=[
